8e5c953031cf09d6271b233635e8b0ff0ff3236b
unknown <saraazabel@gmail.com>
2020-11-02 20:46:01 -0300

    test

--- cipher.js
+++ cipher.js
@@ -5,6 +5,7 @@ const cipher = {
 export default cipher;
 //[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
 //[65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90]
+//[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 // "A".charCodeAt(0) --> 65
 //String.fromCharCode(72) -->H
 // ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
