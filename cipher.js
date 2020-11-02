5298861cccb49ae17c65d5d87d46e7c1134d63e0
unknown <saraazabel@gmail.com>
2020-11-02 15:08:25 -0300

    test

--- cipher.js
+++ cipher.js
@@ -3,3 +3,33 @@ const cipher = {
 };
 
 export default cipher;
+//[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+//[65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90]
+// "A".charCodeAt(0) --> 65
+//String.fromCharCode(72) -->H
+// ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+//codigoASC -> codigo0a25 -> desloc -> giro -> codigoASC
+//               -65           +desl    %tam      +65
+function encode(offset, string) {
+  offset = offset % 26;
+  var maiusculas = string.toUpperCase();
+  var alfabeto = "abcdefghijklmnopqrstuvwxyz".split(" ");
+  var novaString = " ";
+  for (let crip = 0; crip<string.length; crip++){
+    var letraAtual = maiusculas[crip];
+    if(letraAtual === " ") {
+      novaString += letraAtual;
+      continue;
+    }
+    var indiceAtual = alfabeto.indexOf(letraAtual);
+    var novoIndice = indiceAtual + offset;
+    if (novoIndice> 25 ) novoIndice = novoIndice - 26;
+    if (novoIndice< 26 ) novoIndice = novoIndice + 26;
+    if (string[crip] === string[crip].toUpperCase()) {
+      novaString += alfabeto[novoIndice].toUpperCase();
+    }
+    else novaString += alfabeto[novoIndice];
+  }
+  return novaString
+
+}
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+/*dom
 import cipher from './cipher.js';
 
-console.log(cipher);
+console.log(cipher);*/
+const button = document.getElementById("button")
+button.addEventListener("click", crip)
+
+function crip() {
+    var deslocamento = Number(document.getElementById("desloc").value);
+    var msg =  document.getElementById("mensagem").value;
+    var resu = deslocamento + msg;
+
+    document.getElementById("result").innerHTML = resu;
+
+}
+
+
